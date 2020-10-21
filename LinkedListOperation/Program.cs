b3b4b1e0d5588f00b6acc7e18663c64b46b0b02e
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-21 23:24:00 +0530

    [SUNNY]Add.Welcome to linked list implementation

---
+++ LinkedListOperation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Implementation!");
+        }
+    }
+}
